b215b8843660c98e000106d7d1df66e64d7b94b2
shubhamarjun5@gmail.com <shubhamarjun5@gmail.com>
2025-05-30 08:46:03 +0530

    again commiting on 29 may

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
   return (
-    <div className='app-download' id='app-donload'>
+    <div className='app-download' id='app-download'>
         <p>For Better Experience Download <br /> Tomato App</p>
         <div className="app-download-platforms">
             <img src={assets.play_store} alt="" />
