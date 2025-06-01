984b7483e6a088763cd1914715ca0e78dd9a230a
shubhamarjun5@gmail.com <shubhamarjun5@gmail.com>
2025-06-01 14:29:13 +0530

    commitin on 1 june

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
 
+
 const Home = () => {
 
    const [category,setCategory]=useState("All");
